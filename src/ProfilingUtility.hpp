e556ae5f49c306b085bafba4368b407fd09c70d7
DNKpp <dnkpp2011@gmail.com>
2024-02-21 13:53:25 +0100

    slightly adjust ProfilingUtility.hpp

--- src/ProfilingUtility.hpp
+++ src/ProfilingUtility.hpp
@@ -5,12 +5,20 @@
 
 using RealSeconds = std::chrono::duration<double>;
 
+void do_not_optimize(auto& value)
+{
+	volatile auto* var = std::addressof(value);
+}
+
 template <typename Fun, typename... Args>
 	requires std::invocable<Fun, Args...>
 constexpr RealSeconds profile(Fun fun, Args&&... inputs)
 {
 	const auto begin = std::chrono::steady_clock::now();
-	volatile auto result = std::invoke(fun, std::forward<Args>(inputs)...);
+	auto result = std::invoke(fun, std::forward<Args>(inputs)...);
+	const auto duration = std::chrono::steady_clock::now() - begin;
+	do_not_optimize(result);
 
-	return std::chrono::duration_cast<RealSeconds>(std::chrono::steady_clock::now() - begin);
+	return std::chrono::duration_cast<RealSeconds>(duration);
 }
+
